884c9d733c39dc03912c3e18e8ecc71320292e3c
Shashank <shashankkumarrup5@gmail.com>
2026-06-02 13:24:57 +0530

    Frontend Added using EJS

--- controllers/url.js
+++ controllers/url.js
@@ -12,7 +12,11 @@ async function handleGenerateNewShortURL(req, res)
     visitedHistory: [],
 
   });
-  return res.json({id: shortID});
+
+  return res.render('home', {
+    id:shortID,
+  });
+  // return res.json({id: shortID});
 }
 
 async function handleGetAnalytics(req, res){
--- index.js
+++ index.js
@@ -1,15 +1,66 @@
 import express from "express";
+import path from "path";
 import urlRoute from "./routes/url.js";
 import URL from "./models/url.js";
 import connectToMongoDB from "./connect.js";
+import staticRoute from "./routes/staticRouter.js"
+
 const app = express();
 
 const PORT = 8001;
 
-app.use(express.json())
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use(express.static("public"));
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); 
+
+app.use("/url", urlRoute);
+app.use("/", staticRoute);
+
+
+
+// ejs, pug, handlebars etc. (Templeting engine)  works as server side rendering
+
+// Server Side rendering
+
+// app.get('/test', async (req, res) => {
+//   const allUrls = await URL.find({});
+//   return res.end(`
+//     <html>
+//       <head> </head>
+//       <body>
+//         <ol>
+//         ${allUrls.map(url => `<li>${url.shortId} - ${url.redirectURL} - Total Visit = ${url.visitHistory.length} </li>`)}.join('')
+//         </ol>
+        
+//       </body>
+//     </html>
+//     `);
+// });
+
+// alternate way
+
+// app.get('/test', async (req, res) => {
+//   const allUrls = await URL.find({});
+//   return res.render('home', {
+//     urls: allUrls,
+//   });
+
+// });
+
+
+
+
 app.use("/url", urlRoute);
 
-app.get('/:shortID', async (req, res) => {
+
+
+
+app.get('/url/:shortID', async (req, res) => {
   const shortId = req.params.shortID;
   const entry = await URL.findOneAndUpdate({
     shortId
@@ -28,5 +79,5 @@ app.get('/:shortID', async (req, res) => {
 connectToMongoDB('mongodb://localhost:27017/short-url').then(() => { console.log("MongoDB connected.") });
 
 app.listen(PORT, () => {
-  console.log(`Server Started at PORT: ${PORT} \n http://localhost/${PORT}`)
+  console.log(`Server Started at PORT: ${PORT} \n http://localhost:${PORT}`)
 })
